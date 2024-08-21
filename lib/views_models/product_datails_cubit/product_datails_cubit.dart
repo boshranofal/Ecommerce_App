e22089e0dfb31016e0f85e78fc5866f78200ed6a
boshranofal <boshranofal66@gmail.com>
2024-08-21 14:20:57 +0300

    add product cubit

--- lib/views_models/product_datails_cubit/product_datails_cubit.dart
+++ lib/views_models/product_datails_cubit/product_datails_cubit.dart
@@ -22,4 +22,35 @@ class ProductDatailsCubit extends Cubit<ProductDatailsState> {
     product.decrementCounter();
     emit(ProductDatailsLoaded(product: product));
   }
+
+  void selectColor(ProductModels product, int index) {
+    product.selectColor(index);
+    emit(ProductDatailsLoaded(product: product));
+  }
+
+  void addcart() {
+    emit(Adding());
+    Future.delayed(const Duration(seconds: 1), () {
+      final addCart = dummyCart;
+      emit(Added(product: addCart));
+    });
+  }
+
+  void togglecart(ProductModels product) {
+    //emit(ProductDatailsLoaded(product: product));
+    Future.delayed(const Duration(milliseconds: 500), () {
+      if (!dummyCart.contains(product)) {
+        dummyCart.add(product);
+        emit(ProductDatailsLoaded(product: product));
+      }
+    });
+  }
+
+  void remove(ProductModels product) {
+    emit(Adding());
+    Future.delayed(const Duration(milliseconds: 100), () {
+      dummyCart.remove(product);
+      emit(Added(product: dummyCart));
+    });
+  }
 }
--- lib/views_models/product_datails_cubit/product_datails_state.dart
+++ lib/views_models/product_datails_cubit/product_datails_state.dart
@@ -23,3 +23,36 @@ final class QuantityState extends ProductDatailsState {
 
   QuantityState({required this.quantity});
 }
+
+final class ProductDatailsAddToCart extends ProductDatailsState {
+  final ProductModels product;
+
+  ProductDatailsAddToCart({required this.product});
+}
+
+final class Adding extends ProductDatailsState {}
+
+final class Added extends ProductDatailsState {
+  final List<ProductModels> product;
+
+  Added({required this.product});
+}
+
+final class AddError extends ProductDatailsState {
+  final String message;
+
+  AddError({required this.message});
+}
+
+final class SetCartAdding extends ProductDatailsState {
+  final String id;
+
+  SetCartAdding({required this.id});
+}
+
+final class SetCartAdded extends ProductDatailsState {
+  final String id;
+  final bool isCart;
+
+  SetCartAdded({required this.id, required this.isCart});
+}
